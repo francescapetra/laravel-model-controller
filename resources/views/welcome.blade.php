e1fd77f3edea0964dea6cd51f360f24ad3c072a7
francescapetra <francescalaurapetra@gmail.com>
2021-05-13 16:36:35 +0200

    foreach movie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,19 +81,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Movies
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @foreach ($movies as $movie)
+                <h2>{{$movie->title}}</h2>
+                <p>{{$movie->author}}</p>
+                <p>{{$movie->genre}}</p>
+                <p>{{$movie->plot}}</p>    
+                @endforeach
+            </div>
             </div>
         </div>
     </body>
