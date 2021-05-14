8524b60c89effda6812b9802a31bea4782fb95a7
francescapetra <francescalaurapetra@gmail.com>
2021-05-14 16:45:43 +0200

    nuovo controller, set della route, update migration

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Movies</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -64,20 +64,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <div class="flex-center position-ref full-height"></div>
 
             <div class="content">
                 <div class="title m-b-md">
@@ -93,4 +80,3 @@
         </div>
     </body>
 </html>
-
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'MovieController@index');
+Route::resource('movies', 'MovieController');
+
