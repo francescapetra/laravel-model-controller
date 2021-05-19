f7f2bd6f68675cf6dd5ff192327cf51e2beba0fd
francescapetra <francescalaurapetra@gmail.com>
2021-05-19 16:34:56 +0200

    return nella welcome

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('movies', 'MovieController');
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home','HomeController@index')->name('home');
